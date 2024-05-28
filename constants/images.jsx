e669a974339ec05ac2a7ef1cf86265daeff1abc0
Beacon-creator <adebiyibukola157@gmail.com>
2024-05-28 11:00:57 +0100

    new commit

--- constants/images.jsx
+++ constants/images.jsx
@@ -1,6 +1,6 @@
-import logo1 from '../assets/fanal.jpg';
-import logo2 from '../assets/backicon.png';
-import logo3 from "../assets/LogoOtter.png";
+import logo1 from '../assets/fanal.png';
+import logo2 from '../assets/fanal.png';
+import logo3 from "../assets/fanale.png";
 import onboarding1 from '../assets/onboarding1.png';
 import Unicorn from '../assets/Unicorn.png';
 export const images = {
